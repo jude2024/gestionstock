eed6c2361ad704f09648fe8936060f252bcfc9a6
Jude <houansoujude1@gmail..com>
2025-09-27 02:03:06 +0200

    m

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,13 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>StockManager - @yield('title', 'Dashboard')</title>
 
-    <a class="navbar-brand d-flex align-items-center" href="{{ route('dashboard') }}">
-        <img src="{{ asset('logo.png') }}" alt="StockManager" style="height:40px; width:auto; margin-right:10px;">
-        StockManager
-    </a>
-
-
-
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
